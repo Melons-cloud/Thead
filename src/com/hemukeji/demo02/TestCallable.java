873baeb564d0147cba1f812b03bc9046c005a39c
hjl19910901 <18710360575@163.com>
2020-02-20 22:21:25 +0800

    线程

--- src/com/hemukeji/demo02/TestCallable.java
+++ src/com/hemukeji/demo02/TestCallable.java
@@ -11,52 +11,8 @@ import java.util.concurrent.*;
 
 //线程创建，实现Callable接口
 public class TestCallable implements Callable<Boolean> {
-
     private String url;//网络图片地址
     private String name;
-
-
-    public TestCallable(String url,String name){
-
-        this.url=url;
-        this.name=name;
-    }
-    @Override
-    public Boolean call(){
-
-       WebDownloader webDownloader =new WebDownloader();
-        webDownloader.downloader(url,name);
-        System.out.println("下载了文件名为:"+name);
-        return true;
-
-    }
-    public static void main(String[] args) throws ExecutionException,InterruptedException {
-
-
-        TestCallable t1 =new TestCallable("https://blog.kuangstudy.com/usr/uploads/2019/10/3278965235.png","1.jpg");
-        TestCallable t2 =new TestCallable("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1582214272633&di=e8765d1493083ebbae9aaaef0c37a610&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20180209%2Ff4734a37574746da8a7c27906e77eaa5.jpeg","2.jpg");
-        TestCallable t3 =new TestCallable("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1582214272632&di=ad274a869bab08456ffca0fa6bbdae13&imgtype=0&src=http%3A%2F%2Fimg.qzcns.com%2F2018%2F0521%2F20180521092124931.jpg","3.jpg");
-        //创建执行服务
-        ExecutorService ser = Executors.newFixedThreadPool(3);
-
-        //提交执行
-        Future<Boolean> r1 = ser.submit(t1);
-        Future<Boolean> r2 = ser.submit(t2);
-        Future<Boolean> r3 = ser.submit(t3);
-
-        //获取结果
-        boolean rs1=r1.get();
-        boolean rs2=r1.get();
-        boolean rs3=r1.get();
-
-        //关闭服务
-        ser.shutdownNow();
-
-
-
-
-
-    }
 
 
     class WebDownloader{
@@ -76,5 +32,37 @@ public class TestCallable implements Callable<Boolean> {
 
 
     }
+
+    public TestCallable(String url,String name){
+        this.url=url;
+        this.name=name;
+    }
+    @Override
+    public Boolean call(){
+        WebDownloader webDownloader =new WebDownloader();
+        webDownloader.downloader(url,name);
+        System.out.println("下载了文件名为:"+name);
+        return true;
+
+    }
+    public static void main(String[] args) throws ExecutionException,InterruptedException {
+        TestCallable t1 =new TestCallable("https://blog.kuangstudy.com/usr/uploads/2019/10/3278965235.png","1.jpg");
+        TestCallable t2 =new TestCallable("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1582214272633&di=e8765d1493083ebbae9aaaef0c37a610&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20180209%2Ff4734a37574746da8a7c27906e77eaa5.jpeg","2.jpg");
+        TestCallable t3 =new TestCallable("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1582214272632&di=ad274a869bab08456ffca0fa6bbdae13&imgtype=0&src=http%3A%2F%2Fimg.qzcns.com%2F2018%2F0521%2F20180521092124931.jpg","3.jpg");
+        //创建执行服务
+        ExecutorService ser = Executors.newFixedThreadPool(3);
+        //提交执行
+        Future<Boolean> r1 = ser.submit(t1);
+        Future<Boolean> r2 = ser.submit(t2);
+        Future<Boolean> r3 = ser.submit(t3);
+        //获取结果
+        boolean rs1=r1.get();
+        boolean rs2=r1.get();
+        boolean rs3=r1.get();
+        //关闭服务
+        ser.shutdownNow();
+    }
+
+
 
 }
