05ab31d86332e3d3779d2a7af441efc45f575dc3
hjl19910901 <18710360575@163.com>
2020-02-20 21:12:27 +0800

    线程

--- src/com/hemukeji/demo1/TestThread1.java
+++ src/com/hemukeji/demo1/TestThread1.java
@@ -1,4 +1,33 @@
 package com.hemukeji.demo1;
 
-public class TestThread1 {
+//线程开启不一定立即执行，由CPU调度执行。由run()方法先执行，无随机执行。
+//创建线程的方式:继承Thread类，重写run()方法,调用start()方法开启线程
+public class TestThread1  extends Thread{
+
+    @Override
+    public void run(){
+
+      //run方法，线程体
+
+        for (int i=0;i<200;i++){
+
+            System.out.println("我在看代码---"+i);
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+        //main主线程
+
+        TestThread1 testThread1 = new TestThread1();
+        testThread1.start();
+
+        for (int i=0;i<1000;i++){
+
+            System.out.println("我在学习多线程"+i);
+        }
+
+    }
 }
---
+++ src/com/hemukeji/demo1/TestThread2.java
@@ -0,0 +1,63 @@
+package com.hemukeji.demo1;
+
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+//Thread，实现多线程同步下载图片
+public class TestThread2 extends Thread {
+
+    private String url;//网络图片地址
+    private String name;
+
+
+    public TestThread2(String url,String name){
+
+        this.url=url;
+        this.name=name;
+    }
+    @Override
+    public void run(){
+
+        WebDownloader webDownloader =new WebDownloader();
+        webDownloader.downloader(url,name);
+        System.out.println("下载了文件名为:"+name);
+
+    }
+    public static void main(String[] args) {
+
+
+        TestThread2 t1 =new TestThread2("https://blog.kuangstudy.com/usr/uploads/2019/10/3278965235.png","1.jpg");
+        TestThread2 t2 =new TestThread2("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1582214272633&di=e8765d1493083ebbae9aaaef0c37a610&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20180209%2Ff4734a37574746da8a7c27906e77eaa5.jpeg","2.jpg");
+        TestThread2 t3=new TestThread2("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1582214272632&di=ad274a869bab08456ffca0fa6bbdae13&imgtype=0&src=http%3A%2F%2Fimg.qzcns.com%2F2018%2F0521%2F20180521092124931.jpg","3.jpg");
+        t1.start();
+        t2.start();
+        t3.start();
+
+
+    }
+
+
+    class WebDownloader{
+
+        //下载方法
+        public void downloader( String url,String name){
+
+
+            try {
+                FileUtils.copyURLToFile(new URL(url),new File(name));
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("IO异常,downloader方法出现问题");
+            }
+
+        }
+
+
+    }
+
+
+}
